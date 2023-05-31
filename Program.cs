a70f58635eb14e73b0d75fea5b9e8756a94d6bda
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-05-30 17:29:47 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prog_120_S23_L15_ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        } // main
+
+    } // class
+
+} // namespace
